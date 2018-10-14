9fbc57813c365d0c81a6e21e48c033a583b3ac0b
Alessio-ubuntu <alessios.18@gmail.com>
2018-10-14 14:59:01 +0200

    Project base

---
+++ src/main/java/it/alessios/jtaskslogger/App.java
@@ -0,0 +1,13 @@
+package it.alessios.jtaskslogger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
